d74f34353d2e18339ce6e28069ebcc1db377c948
jimmy <jimeee0e.gmail.com>
2023-01-27 23:21:54 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ2RXBo-LMOjNFNxMBcNywAMfiqOPil5Q",
+  authDomain: "jwitter-da9ec.firebaseapp.com",
+  projectId: "jwitter-da9ec",
+  storageBucket: "jwitter-da9ec.appspot.com",
+  messagingSenderId: "503173396293",
+  appId: "1:503173396293:web:ec0cf8c76b694162b8b02c",
+};
+
+export const app = initializeApp(firebaseConfig);
