2e38d1ae6ca4fd328d6c415c6adecef211462116
jimmy <jimeee0e.gmail.com>
2023-02-02 20:12:05 +0900

    creating account, Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
-import { authService } from "fbase";
+// import { authService } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); //유저의 로그인 여부를 알 수 있게 됨.
-  return (
-    <>
-      <AppRouter isLoggedIn={isLoggedIn} />
-      <footer>&copy; Jwitter {new Date().getFullYear()}</footer>
-    </>
-  );
+  const [init, setInit] = useState(false); //초기화 전 상태
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //어떻게 기다리냐면
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setIsLoggedIn(true);
+        const uid = user.uid;
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  <>
+    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "잠시만 기다려봐"}
+    <footer>&copy; Jwitter {new Date().getFullYear()}</footer>
+  </>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,16 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
-
+  const [error, setError] = useState("");
   const onChange = event => {
     //심플하게 form을 컨트롤 할 수 있는 방법
     const {
@@ -16,10 +22,29 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault(); //기본 행위가 실행되는 걸 원치 않는다.
+    try {
+      let data;
+      const auth = getAuth();
+      if (newAccount) {
+        //creat account
+        const data = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+      } else {
+        //log in
+        const data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
   };
-
+  //이전 값을 가져와서 그 값에 반대되는 것을 리턴할 거야.
+  const toggleAccount = () => setNewAccount(prev => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -36,14 +61,18 @@ const Auth = () => {
           type="password"
           placeholder="password"
           required
-          value={password} //input값들을 가져다 쓰고 싶기때문
+          value={password}
           onChange={onChange}
         />
         <input
           type="submit"
           value={newAccount ? "Create Account" : "Log In"}
         />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
 
       <div>
         <button>Continue with Google</button>
